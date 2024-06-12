1bccb84822621db2520da1a13587ac077ccaf78f
Lucas09876543210 <162449839+Lucas09876543210@users.noreply.github.com>
2024-06-12 08:21:22 -0300

    Add files via upload

--- Lucas 3B/main.js
+++ Lucas 3B/main.js
@@ -4,40 +4,51 @@ var cxAlternativas=document.querySelector("cxAlternativas")
 var cxResultado =document.querySelector("cxResultados")
 var txResultado =document.querySelector("txResultado")
 var perguntas =[
+
+
     {
-        enunciado = "A IA pode automatizar tarefas repetitivas.",
-        alternativas = [
+        enunciado: "A IA pode automatizar tarefas repetitivas.",
+        alternativas: [
             "Vai eliminar tarefas monotonas",
             "Vai tirar muitos empregos"
         ]
     },
     {
-        enunciado = "A IA pode tomar decisões baseadas em dados.",
-        alternativas = [
+        enunciado: "A IA pode tomar decisões baseadas em dados.",
+        alternativas: [
             "Maravilhoso",
             "Assustador"
         ]
     },
     {
-        enunciado = "A IA pode substituir certos empregos.",
-        alternativas = [
+        enunciado:  "A IA pode substituir certos empregos.",
+        alternativas: [
             "Não vejo problemas",
             "É o fim dos empregos como conhecemos hoje"
         ]
     },
     {
-        enunciado = "A IA pode ajudar a prever tendências de mercado.",
-        alternativas = [
+        enunciado: "A IA pode ajudar a prever tendências de mercado.",
+        alternativas: [
             "Incrível",
             "Sinistro"
         ]
     },
      {
-        enunciado = "A IA pode trabalhar 24/7 sem pausas.",
-        alternativas = [
+        enunciado : "A IA pode trabalhar 24/7 sem pausas.",
+        alternativas: [
             "Surpreendente",
             "Mão de obra barata"
         ]
     }
      
 ]
+var posiçaoAtual = 0;
+var perguntaAtual;
+
+mostraPergunta();
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posiçaoAtual];
+    cxPerguntas.textContent = perguntaAtual.enunciado;
+}
